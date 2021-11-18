89240cdee2ea8d5c18be9a7f941615d2dee63f6d
Yevheniy <peredaryuk.yevheniy@gmail.com>
2021-11-18 17:34:26 +0200

    Created project and done some start things

---
+++ Bomberman/FormGame.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Bomberman
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutAuthorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelGame = new System.Windows.Forms.Panel();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.informationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1306, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "mainMenuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New game";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // informationToolStripMenuItem
+            // 
+            this.informationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutGameToolStripMenuItem,
+            this.aboutAuthorToolStripMenuItem});
+            this.informationToolStripMenuItem.Name = "informationToolStripMenuItem";
+            this.informationToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.informationToolStripMenuItem.Text = "Help";
+            // 
+            // aboutGameToolStripMenuItem
+            // 
+            this.aboutGameToolStripMenuItem.Name = "aboutGameToolStripMenuItem";
+            this.aboutGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutGameToolStripMenuItem.Text = "About game";
+            this.aboutGameToolStripMenuItem.Click += new System.EventHandler(this.aboutGameToolStripMenuItem_Click);
+            // 
+            // aboutAuthorToolStripMenuItem
+            // 
+            this.aboutAuthorToolStripMenuItem.Name = "aboutAuthorToolStripMenuItem";
+            this.aboutAuthorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutAuthorToolStripMenuItem.Text = "About author ";
+            this.aboutAuthorToolStripMenuItem.Click += new System.EventHandler(this.aboutAuthorToolStripMenuItem_Click);
+            // 
+            // panelGame
+            // 
+            this.panelGame.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelGame.Location = new System.Drawing.Point(0, 64);
+            this.panelGame.Name = "panelGame";
+            this.panelGame.Size = new System.Drawing.Size(1306, 571);
+            this.panelGame.TabIndex = 1;
+            // 
+            // labelScore
+            // 
+            this.labelScore.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScore.Location = new System.Drawing.Point(0, 28);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(1306, 33);
+            this.labelScore.TabIndex = 2;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1306, 635);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.panelGame);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " BomberMan";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutAuthorToolStripMenuItem;
+        private System.Windows.Forms.Panel panelGame;
+        private System.Windows.Forms.Label labelScore;
+    }
+}
+
---
+++ Bomberman/FormGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bomberman
+{
+    public partial class FormGame : Form
+    {
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutGameToolStripMenuItem_Click(object sender, EventArgs e)
+            => MessageBox.Show("About Bomberman...");
+
+        private void aboutAuthorToolStripMenuItem_Click(object sender, EventArgs e)
+            => MessageBox.Show("About Author...");
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+            => this.Close();
+    }
+}
